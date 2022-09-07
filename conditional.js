47d7dfd2e8eb847caa779d81a96db1ec88b67336
chijideso <chijideso@gmail.com>
2022-09-07 11:43:37 +0100

    created conditions.j

---
+++ conditional.js
@@ -0,0 +1,40 @@
+// console.log("hello world");
+// the if statement 
+// the else
+// the elseif
+// the switch
+// the tenary operator
+
+
+// if statement 
+if (condtion){
+    // expression
+    // will only run if result is true
+}
+
+let a = "wednesday";
+let b = "september";
+if (a == "wednesday" && b =="june"){
+    //console.log(true)
+} else{
+    // console.log(false)
+}
+
+// the if else
+// it checks fpr another condition when the previos condition if false
+
+if (a == "wednesday" && b =="june"){
+    //console.log(true)
+} else if (a == "wednesday" && b !="june"){
+    console.log(true)
+}
+ else if (a == "wednesday" && b !="september"){
+    console.log(true)
+}
+else{
+    console.log("none of the above")
+}
+
+
+
+    // console.log(false)
